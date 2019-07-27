cc35d44a82c7c046996f822264d0e41982e0027e
Jayden <jayden@chequer.io>
2019-07-27 18:06:38 +0900

    Vue 애니메이션, 모달 기능 구현

--- todo-app/src/components/TodoFooter.vue
+++ todo-app/src/components/TodoFooter.vue
@@ -8,24 +8,24 @@
 export default {
   methods: {
     clearTodo() {
-      this.$emit('removeAll');
+      this.$emit("removeAll");
     }
   }
-}
+};
 </script>
 
 <style>
-  .clearAllContainer {
-    width: 8.5rem;
-    height: 50px;
-    line-height: 50px;
-    background-color: white;
-    border-radius: 5px;
-    margin: 0 auto;
-  }
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
 
-  .clearAllBtn {
-    color: #e20303;
-    display: black;
-  }
+.clearAllBtn {
+  color: #e20303;
+  display: black;
+}
 </style>
--- todo-app/src/components/TodoInput.vue
+++ todo-app/src/components/TodoInput.vue
@@ -1,62 +1,84 @@
 <template>
   <div class="inputBox shadow">
-    <input type="text" v-model="newTodoItem" placeholder="Type What you have to do" v-on:keyup.enter="addTodo">
+    <input
+      type="text"
+      v-model="newTodoItem"
+      placeholder="Type What you have to do"
+      v-on:keyup.enter="addTodo"
+    />
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="body">할일을 입력하세요.</span>
+      <span slot="footer" @click="showModal = false">
+        닫기
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
+
 export default {
   data() {
     return {
-      newTodoItem: ''
-    }
+      newTodoItem: "",
+      showModal: false
+    };
+  },
+  components: {
+    Modal: Modal
   },
   methods: {
     addTodo() {
       if (this.newTodoItem !== "") {
         var value = this.newTodoItem && this.newTodoItem.trim();
         console.log(value);
-        this.$emit('addTodo', value);
+        this.$emit("addTodo", value);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput() {
-      this.newTodoItem = '';
+      this.newTodoItem = "";
     }
   }
-}
+};
 </script>
 
 <style>
-  input:focus {
-    outline: none;
-  }
+input:focus {
+  outline: none;
+}
 
-  .inputBox {
-    background: white;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 5px;
-  }
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
 
-  .inputBox input {
-    border-style: none;
-    font-size: 0.9rem;
-  }
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
 
-  .addContainer {
-    float: right;
-    background: linear-gradient(to right, #6478FB, #8763FB);
-    display: block;
-    width: 3rem;
-    border-radius: 0 5px 5px 0;
-  }
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
 
-  .addBtm {
-    color: white;
-    vertical-align: middle;
-  }
+.addBtm {
+  color: white;
+  vertical-align: middle;
+}
 </style>
